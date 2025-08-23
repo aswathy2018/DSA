be4dfc78c957d44d53d81fc1cc50c9ea7611dcb3
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-08-23 17:08:30 +0530

    Updated

--- Week 3/Tree/bt.js
+++ Week 3/Tree/bt.js
@@ -42,4 +42,59 @@ class BT{
             }
         }
     }
+
+    preOrder(node=this.root){
+        if(node){
+            console.log(node.value)
+            this.preOrder(node.left)
+            this.preOrder(node.right)
+        }
+    }
+
+    inOrder(node=this.root){
+        if(node){
+            this.inOrder(node.left)
+            console.log(node.value)
+            this.inOrder(node.right)
+        }
+    }
+
+    postOrder(node=this.root){
+        if(node){
+            this.postOrder(node.left)
+            this.postOrder(node.right)
+            console.log(node.value)
+        }
+    }
+
+
+    BFS(){
+        let queue = [this.root]
+
+        while(queue.length){
+            let curr = queue.shift()
+            console.log(curr.value)
+            
+            if(curr.left){
+                queue.push(curr.left)
+            }
+
+            if(curr.right){
+                queue.push(curr.right)
+            }
+        }
+    }
 }
+
+
+let tree=new BT()
+
+tree.insert(50)
+tree.insert(7)
+tree.insert(3)
+tree.insert(1)
+tree.insert(9)
+tree.insert(12)
+
+console.log("BFS ");
+tree.BFS()
