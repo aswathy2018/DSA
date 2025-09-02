7c574ef7b15b0a00b8d649ac9af35c482465e48b
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-09-02 17:40:32 +0530

    Updated

--- Week 3/Heap/heapSort.js
+++ Week 3/Heap/heapSort.js
@@ -1,33 +1,34 @@
-function heapSort(arr){
+function heap(arr){
     let n=arr.length
 
-    for(let i=Math.floor(n/2)-1;i>=0;i--){
-        hepify(arr,i,n)
+    for(let i=Math.floor(n/2)-1 ; i>=0 ; i--){
+        heapify(arr,i,n)
     }
-    for(let i=n-1;i>0;i--){
-        [arr[0],arr[i]]=[arr[i],arr[0]]
-        hepify(arr,0,i)
+
+    for(let i=n-1 ; i>0 ; i--){
+        [arr[0], arr[i]] = [arr[i], arr[0]]
+        heapify(arr,0,i)
     }
     return arr
 }
 
-function hepify(arr,index,size){
-    let largest=index
-    let left = (2*index)+1
-    let right=(2*index)+2
+function heapify(arr,index,size){
+    let lar = index
+    let left = 2*index+1
+    let right = 2*index+2
 
-    if(left<size && arr[left]>arr[largest]){
-        largest=left
+    if(left<size && arr[lar]<arr[left]){
+        lar = left
     }
-    if(right<size&& arr[right]>arr[largest]){
-        largest=right
+
+    if(right<size && arr[lar]<arr[right]){
+        lar = right
     }
-    if(largest!==index){
-        [arr[index],arr[largest]]=[arr[largest],arr[index]]
-        hepify(arr,largest,size)
+
+    if(lar!==index){
+        [arr[index], arr[lar]] = [arr[lar], arr[index]]
+        heapify(arr,lar,size)
     }
 }
 
-let arr=[11,2,5,7,4,33,88]
-
-console.log(heapSort(arr));
+console.log(heap([4,1,6,2,8,3,9]))
