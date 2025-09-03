fdb03b61ed23b34aecba6b7595b7c4e9ab208181
aswathy2018 <aswathysudhakaran3002@gmail.com>
2025-09-03 12:05:16 +0530

    Updated

--- Week 3/Tri/trie.js
+++ Week 3/Tri/trie.js
@@ -34,7 +34,7 @@ class Trie{
         return node.isEnd
     }
 
-    startsWith(prefix){
+    startWith(prefix){
         let node = this.root
 
         for(let char of prefix){
@@ -45,4 +45,59 @@ class Trie{
         }
         return true
     }
+
+    autocomplete(prefix){
+        let node = this.root
+        let res = []
+
+        for(let char of prefix){
+            if(!node.child[char]){
+                return res
+            }
+            node = node.child[char]
+        }
+        this.collect(node,prefix,res)
+        return res
+    }
+
+    collect(node, prefix, res){
+        if(node.isEnd){
+            res.push(prefix)
+        }
+
+        for(let char in node.child){
+            this.collect(node.child[char], prefix+char, res)
+        }
+    }
+
+    longestprefix(word){
+        let node = this.root
+        let long = ''
+        let pref = ''
+
+        for(let char of word){
+            if(!node.child[char]){
+                break
+            }
+            node = node.child[char]
+            pref+=char
+        }
+        if(node.isEnd){
+            long+=pref
+        }
+        return long
+    }
 }
+
+
+let val = new Trie()
+
+val.insert("app")
+val.insert("apple")
+val.insert("Orange")
+val.insert("appleSyrup")
+
+console.log("Search: ", val.search("apple"));
+console.log("start with: ", val.startWith("app"));
+console.log("auto: ", val.autocomplete("apple"));
+console.log("longest: ", val.longestprefix("apple"));
